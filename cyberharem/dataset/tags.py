fa44c64badd6fa8f2d005c9d1070731ca007b1fa
narugo1992 <narugo@126.com>
2023-08-23 01:17:37 +0800

    dev(narugo): name, 1.1

--- cyberharem/dataset/tags.py
+++ cyberharem/dataset/tags.py
@@ -31,7 +31,7 @@ generic_neg_words = [
 def _free_pos_words(generic_words, name, core_tags):
     return [
         *generic_words,
-        (name, 1.0),
+        (name, 1.1),
         *[key for key, _ in sorted(core_tags.items(), key=lambda x: -x[1])],
     ], generic_neg_words, None, True
 
@@ -44,7 +44,7 @@ def _bikini_pos_words(generic_words, name, core_tags):
         'beach',
         'beautiful detailed sky',
         ('extremely detailed background', 1.2),
-        (name, 1.0),
+        (name, 1.1),
         ('standing', 1.1),
         'looking at viewer',
         ('bikini', 1.3),
@@ -63,7 +63,7 @@ def _nude_pos_words(generic_words, name, core_tags):
         ('spread legs', 1.1),
         ('arms up', 1.1),
         'mature',
-        (name, 1.0),
+        (name, 1.1),
         *[key for key, _ in sorted(core_tags.items(), key=lambda x: -x[1])],
         'nipples',
         ('pussy', 1.15),
@@ -83,7 +83,7 @@ def _nude_stand_words(generic_words, name, core_tags):
         ('nude', 1.4),
         ('completely nude', 1.2),
         'mature',
-        (name, 1.0),
+        (name, 1.1),
         *[key for key, _ in sorted(core_tags.items(), key=lambda x: -x[1])],
         'nipples',
         ('pussy', 1.15),
@@ -98,7 +98,7 @@ def _safe_maid_words(generic_words, name, core_tags):
         *generic_words,
         ('maid', 1.4),
         ('long maid dress', 1.15),
-        (name, 1.0),
+        (name, 1.1),
         *[key for key, _ in sorted(core_tags.items(), key=lambda x: -x[1])],
     ], [
         'nsfw', 'sexy', 'underwear', 'bra', 'fishnet',
@@ -112,7 +112,7 @@ def _safe_yukata_words(generic_words, name, core_tags):
         *generic_words,
         ('yukata', 1.4),
         ('kimono', 1.2),
-        (name, 1.0),
+        (name, 1.1),
         *[key for key, _ in sorted(core_tags.items(), key=lambda x: -x[1])],
     ], [
         'nsfw', 'sexy', 'underwear', 'bra', 'fishnet',
@@ -128,7 +128,7 @@ def _safe_miko_words(generic_words, name, core_tags):
         ('white kimono', 1.2),
         ('red hakama', 1.2),
         ('wide sleeves', 1.15),
-        (name, 1.0),
+        (name, 1.1),
         *[key for key, _ in sorted(core_tags.items(), key=lambda x: -x[1])],
     ], [
         'nsfw', 'sexy', 'underwear', 'bra', 'fishnet',
@@ -148,7 +148,7 @@ def _safe_suit_words(generic_words, name, core_tags):
         ('black skirt', 1.15),
         ('smoking', 1.2),
         'handsome',
-        (name, 1.0),
+        (name, 1.1),
         *[key for key, _ in sorted(core_tags.items(), key=lambda x: -x[1])],
     ], [
         'nsfw', 'sexy', 'underwear', 'bra', 'fishnet',
@@ -195,7 +195,7 @@ def save_recommended_tags(source, name: str = None, workdir: str = None):
 
         core_tags, feats = load_tags_from_directory(ds_dir)
         for i, f in enumerate(feats, start=1):
-            pos_words = [*generic_words, (name, 1.0), *f.keys()]
+            pos_words = [*generic_words, (name, 1.1), *f.keys()]
             pos_prompt = repr_tags(pos_words)
             neg_prompt = repr_tags(generic_neg_words)
 
