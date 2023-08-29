db9ea8c70391ac935d718565bccf935f731aa7f7
narugo1992 <narugo@126.com>
2023-08-30 00:33:45 +0800

    dev(narugo): fix bug

--- cyberharem/publish/civitai.py
+++ cyberharem/publish/civitai.py
@@ -480,13 +480,14 @@ def try_find_title(char_name, game_name):
 
 def try_get_title_from_repo(repo):
     hf_fs = get_hf_fs()
-    if hf_fs.exists(f'dataset/{repo}/meta.json'):
-        data = json.loads(hf_fs.read_text(f'dataset/{repo}/meta.json'))
+    print(f'datasets/{repo}/meta.json')
+    if hf_fs.exists(f'datasets/{repo}/meta.json'):
+        data = json.loads(hf_fs.read_text(f'datasets/{repo}/meta.json'))
         character_name = data['name']
 
         source_name = repo.split('_')[-1]
-        if hf_fs.exists(f'dataset/BangumiBase/{source_name}/meta.json'):
-            base_data = json.loads(hf_fs.read_text(f'dataset/BangumiBase/{source_name}/meta.json'))
+        if hf_fs.exists(f'datasets/BangumiBase/{source_name}/meta.json'):
+            base_data = json.loads(hf_fs.read_text(f'datasets/BangumiBase/{source_name}/meta.json'))
             source_full_name = base_data['name']
             return f'{character_name} ({source_full_name})'
         else:
