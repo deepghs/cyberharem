77fe8f66cb8248e85c3d87b3b9e97b1d3bef9044
narugo1992 <narugo992@gmail.com>
2024-01-26 00:37:15 +0800

    dev(narugo): remove useless code

--- cyberharem/publish/__main__.py
+++ cyberharem/publish/__main__.py
@@ -1,4 +1,3 @@
-import os
 from functools import partial
 
 import click
@@ -6,13 +5,7 @@ from ditk import logging
 from gchar.generic import import_generic
 from gchar.utils import GLOBAL_CONTEXT_SETTINGS
 from gchar.utils import print_version as _origin_print_version
-from hbutils.system import TemporaryDirectory
-from huggingface_hub import hf_hub_url
-from tqdm.auto import tqdm
 
-from cyberharem.dataset import save_recommended_tags
-from cyberharem.publish import find_steps_in_workdir
-from cyberharem.utils import get_hf_fs, download_file
 from .civitai import civitai_publish_from_hf
 from .huggingface import deploy_to_huggingface
 from ..infer.draw import _DEFAULT_INFER_MODEL
@@ -33,87 +26,29 @@ def cli():
               help='Work directory for experiment.', show_default=True)
 @click.option('--repository', '-r', 'repository', type=str, default=None,
               help='Repository to publish to.', show_default=True)
-@click.option('--revision', '-R', 'revision', type=str, default='main',
-              help='Revision for pushing the model.', show_default=True)
-@click.option('-n', '--n_repeats', 'n_repeats', type=int, default=3,
-              help='N Repeats for text encoder', show_default=True)
 @click.option('-m', '--pretrained_model', 'pretrained_model', type=str, default=_DEFAULT_INFER_MODEL,
               help='Pretrained model for preview drawing.', show_default=True)
-@click.option('--width', 'width', type=int, default=512,
+@click.option('--width', 'width', type=int, default=832,
               help='Width of images.', show_default=True)
-@click.option('--height', 'height', type=int, default=768,
+@click.option('--height', 'height', type=int, default=1216,
               help='Height of images.', show_default=True)
 @click.option('-C', '--clip_skip', 'clip_skip', type=int, default=2,
               help='Clip skip.', show_default=True)
 @click.option('-S', '--infer_steps', 'infer_steps', type=int, default=30,
               help='Steps of inference.', show_default=True)
-def huggingface(workdir: str, repository, revision, n_repeats, pretrained_model,
-                width, height, clip_skip, infer_steps):
+def huggingface(workdir: str, repository, pretrained_model, width, height, clip_skip, infer_steps):
     logging.try_init_root(logging.INFO)
     deploy_to_huggingface(
-        workdir, repository, revision, n_repeats, pretrained_model,
-        clip_skip, width, height, infer_steps,
+        workdir=workdir,
+        repository=repository,
+        eval_cfgs={
+            'pretrained_model': pretrained_model,
+            'clip_skip': clip_skip,
+            'infer_steps': infer_steps,
+            'width': width,
+            'height': height,
+        },
     )
-
-
-@cli.command('rehf', context_settings={**GLOBAL_CONTEXT_SETTINGS}, help='Re-Publish to huggingface')
-@click.option('--repository', '-r', 'repository', type=str, default=None,
-              help='Repository to publish to.', show_default=True)
-@click.option('--revision', '-R', 'revision', type=str, default='main',
-              help='Revision for pushing the model.', show_default=True)
-@click.option('-n', '--n_repeats', 'n_repeats', type=int, default=3,
-              help='N Repeats for text encoder', show_default=True)
-@click.option('-m', '--pretrained_model', 'pretrained_model', type=str, default=_DEFAULT_INFER_MODEL,
-              help='Pretrained model for preview drawing.', show_default=True)
-@click.option('--width', 'width', type=int, default=512,
-              help='Width of images.', show_default=True)
-@click.option('--height', 'height', type=int, default=768,
-              help='Height of images.', show_default=True)
-@click.option('-C', '--clip_skip', 'clip_skip', type=int, default=2,
-              help='Clip skip.', show_default=True)
-@click.option('-S', '--infer_steps', 'infer_steps', type=int, default=30,
-              help='Steps of inference.', show_default=True)
-def rehf(repository, revision, n_repeats, pretrained_model,
-         width, height, clip_skip, infer_steps):
-    logging.try_init_root(logging.INFO)
-    with TemporaryDirectory() as workdir:
-        logging.info(f'Downloading models for {workdir!r} ...')
-        hf_fs = get_hf_fs()
-        for f in tqdm(hf_fs.glob(f'{repository}/*/raw/*')):
-            rel_file = os.path.relpath(f, repository)
-            local_file = os.path.join(workdir, 'ckpts', os.path.basename(rel_file))
-            if os.path.dirname(local_file):
-                os.makedirs(os.path.dirname(local_file), exist_ok=True)
-            download_file(
-                hf_hub_url(repository, filename=rel_file),
-                local_file
-            )
-
-        logging.info(f'Regenerating tags for {workdir!r} ...')
-        pt_name, _ = find_steps_in_workdir(workdir)
-        game_name = pt_name.split('_')[-1]
-        name = '_'.join(pt_name.split('_')[:-1])
-
-        from gchar.games.dispatch.access import GAME_CHARS
-        if game_name in GAME_CHARS:
-            ch_cls = GAME_CHARS[game_name]
-            ch = ch_cls.get(name)
-        else:
-            ch = None
-
-        if ch is None:
-            source = repository
-        else:
-            source = ch
-
-        logging.info(f'Regenerate tags for {source!r}, on {workdir!r}.')
-        save_recommended_tags(source, name=pt_name, workdir=workdir)
-        logging.info('Success!')
-
-        deploy_to_huggingface(
-            workdir, repository, revision, n_repeats, pretrained_model,
-            clip_skip, width, height, infer_steps,
-        )
 
 
 @cli.command('civitai', context_settings={**GLOBAL_CONTEXT_SETTINGS}, help='Publish to huggingface')
--- cyberharem/publish/huggingface.py
+++ cyberharem/publish/huggingface.py
@@ -3,7 +3,6 @@ import glob
 import json
 import math
 import os
-import pathlib
 import shutil
 import time
 import zipfile
@@ -15,16 +14,14 @@ from ditk import logging
 from hbutils.string import plural_word
 from hbutils.system import TemporaryDirectory
 from hfutils.operate import upload_directory_as_directory
-from huggingface_hub import CommitOperationAdd, CommitOperationDelete, hf_hub_url
-from huggingface_hub.utils import RepositoryNotFoundError
+from huggingface_hub import CommitOperationAdd, hf_hub_url
 from imgutils.sd import get_sdmeta_from_image
 from tqdm import tqdm
 
 from .convert import convert_to_webui_lora, pack_to_bundle_lora
-from .export import export_workdir, _GITLFS, EXPORT_MARK
-from .steps import find_steps_in_workdir
+from .export import _GITLFS, EXPORT_MARK
 from ..eval import eval_for_workdir, list_steps
-from ..infer.draw import _DEFAULT_INFER_MODEL, list_rtag_names
+from ..infer.draw import list_rtag_names
 from ..utils import get_hf_client, get_hf_fs
 
 
@@ -77,8 +74,8 @@ def _dict_prune(d):
         return d
 
 
-def deploy_to_hf(workdir: str, repository: Optional[str] = None, eval_cfgs: Optional[dict] = None,
-                 steps_batch_size: int = 10):
+def deploy_to_huggingface(workdir: str, repository: Optional[str] = None, eval_cfgs: Optional[dict] = None,
+                          steps_batch_size: int = 10):
     eval_dir = os.path.join(workdir, 'eval')
     if os.path.exists(os.path.join(eval_dir, 'metrics_selected.csv')):
         logging.info(f'Completed evaluation detected on {eval_dir!r}.')
@@ -359,108 +356,4 @@ def deploy_to_hf(workdir: str, repository: Optional[str] = None, eval_cfgs: Opti
             repo_type='model',
             message=f'Upload model for {meta_info["display_name"]}',
             clear=True,
-        )
-
-
-def deploy_to_huggingface(workdir: str, repository=None, revision: str = 'main', n_repeats: int = 3,
-                          pretrained_model: str = _DEFAULT_INFER_MODEL, clip_skip: int = 2,
-                          image_width: int = 512, image_height: int = 768, infer_steps: int = 30,
-                          lora_alpha: float = 0.85, sample_method: str = 'DPM++ 2M Karras',
-                          model_hash: Optional[str] = None):
-    name, _ = find_steps_in_workdir(workdir)
-    repository = repository or f'CyberHarem/{name}'
-
-    logging.info(f'Initializing repository {repository!r} ...')
-    hf_client = get_hf_client()
-    hf_fs = get_hf_fs()
-    if not hf_fs.exists(f'{repository}/.gitattributes'):
-        hf_client.create_repo(repo_id=repository, repo_type='model', exist_ok=True)
-
-    if not hf_fs.exists(f'{repository}/.gitattributes') or \
-            '*.png filter=lfs diff=lfs merge=lfs -text' not in hf_fs.read_text(f'{repository}/.gitattributes'):
-        logging.info(f'Preparing for lfs attributes of repository {repository!r}.')
-        with TemporaryDirectory() as td:
-            _git_attr_file = os.path.join(td, '.gitattributes')
-            with open(_git_attr_file, 'w', encoding='utf-8') as f:
-                print(_GITLFS, file=f)
-
-            operations = [
-                CommitOperationAdd(
-                    path_in_repo='.gitattributes',
-                    path_or_fileobj=_git_attr_file,
-                )
-            ]
-
-            current_time = datetime.datetime.now().astimezone().strftime('%Y-%m-%d %H:%M:%S %Z')
-            commit_message = f'Update {name}\'s .gitattributes, on {current_time}'
-            logging.info(f'Updating {name}\'s .gitattributes to repository {repository!r} ...')
-            hf_client.create_commit(
-                repository,
-                operations,
-                commit_message=commit_message,
-                repo_type='model',
-                revision=revision,
-            )
-
-    with TemporaryDirectory() as td:
-        export_workdir(
-            workdir, td, n_repeats, pretrained_model,
-            clip_skip, image_width, image_height, infer_steps,
-            lora_alpha, sample_method, model_hash, repository,
-        )
-
-        try:
-            hf_client.repo_info(repo_id=repository, repo_type='dataset')
-        except RepositoryNotFoundError:
-            has_dataset_repo = False
-        else:
-            has_dataset_repo = True
-
-        readme_text = pathlib.Path(os.path.join(td, 'README.md')).read_text(encoding='utf-8')
-        with open(os.path.join(td, 'README.md'), 'w', encoding='utf-8') as f:
-            print('---', file=f)
-            print('license: mit', file=f)
-            if has_dataset_repo:
-                print('datasets:', file=f)
-                print(f'- {repository}', file=f)
-            print('pipeline_tag: text-to-image', file=f)
-            print('tags:', file=f)
-            print('- art', file=f)
-            print('---', file=f)
-            print(f'', file=f)
-            print(readme_text, file=f)
-
-        _exist_files = [os.path.relpath(file, repository) for file in hf_fs.glob(f'{repository}/**')]
-        _exist_ps = sorted([(file, file.split('/')) for file in _exist_files], key=lambda x: x[1])
-        pre_exist_files = set()
-        for i, (file, segments) in enumerate(_exist_ps):
-            if i < len(_exist_ps) - 1 and segments == _exist_ps[i + 1][1][:len(segments)]:
-                continue
-            if file != '.':
-                pre_exist_files.add(file)
-
-        operations = []
-        for directory, _, files in os.walk(td):
-            for file in files:
-                filename = os.path.abspath(os.path.join(td, directory, file))
-                file_in_repo = os.path.relpath(filename, td)
-                operations.append(CommitOperationAdd(
-                    path_in_repo=file_in_repo,
-                    path_or_fileobj=filename,
-                ))
-                if file_in_repo in pre_exist_files:
-                    pre_exist_files.remove(file_in_repo)
-        logging.info(f'Useless files: {sorted(pre_exist_files)} ...')
-        for file in sorted(pre_exist_files):
-            operations.append(CommitOperationDelete(path_in_repo=file))
-
-        current_time = datetime.datetime.now().astimezone().strftime('%Y-%m-%d %H:%M:%S %Z')
-        commit_message = f'Publish {name}\'s lora, on {current_time}'
-        logging.info(f'Publishing {name}\'s lora to repository {repository!r} ...')
-        hf_client.create_commit(
-            repository,
-            operations,
-            commit_message=commit_message,
-            repo_type='model',
-            revision=revision,
         )
