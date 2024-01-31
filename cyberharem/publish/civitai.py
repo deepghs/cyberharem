8b7cb46186f88205362c6b4ffc9a3d27670e6a73
narugo1992 <narugo992@gmail.com>
2024-01-31 19:26:15 +0800

    dev(narugo): smoothen the limit

--- cyberharem/publish/civitai.py
+++ cyberharem/publish/civitai.py
@@ -94,7 +94,7 @@ def civitai_upload_from_hf(repository: str, step: Optional[int] = None, allow_ns
 
         df = df[df['ccip'] >= (metrics_info['ccip'] - 0.05)]
         df = df[df['bp'] >= (metrics_info['bp'] - 0.05)]
-        df = df[df['aic'] >= max(metrics_info['aic'] - 0.3, metrics_info['aic'])]
+        df = df[df['aic'] >= max(metrics_info['aic'] - 0.3, metrics_info['aic'] * 0.5)]
 
         df['ccip_x'] = np.round(df['ccip'] * 30) / 30.0
         df['face_x'] = np.round(df['face'] * 20) / 20.0
