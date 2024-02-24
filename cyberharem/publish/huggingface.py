2cdb99bab0313d09ca682d11aec3b61d96e44eef
narugo1992 <narugo992@gmail.com>
2024-02-24 23:35:09 +0800

    dev(narugo): remove unnecessary escape

--- cyberharem/publish/huggingface.py
+++ cyberharem/publish/huggingface.py
@@ -10,7 +10,6 @@ import zipfile
 from textwrap import dedent
 from typing import Optional
 
-import markdown_strings
 import numpy as np
 import pandas as pd
 from discord_webhook import DiscordWebhook
@@ -483,7 +482,7 @@ def publish_to_discord(repository: str, max_cnt: Optional[int] = 10):
             content=textwrap.dedent(f"""
                 Model of `{meta_info['display_name']}` has been published to huggingface repository: {hf_url}.
                 * The base model used for training is [{train_pretrained_model}](https://huggingface.co/{train_pretrained_model}).
-                * Dataset used for training is the `{dataset_info["name"]}` in [{markdown_strings.esc_format(dataset_info["repository"])}](https://huggingface.co/datasets/{dataset_info["repository"]}), which contains {plural_word(dataset_info["size"], "image")}.
+                * Dataset used for training is the `{dataset_info["name"]}` in [{dataset_info["repository"]}](https://huggingface.co/datasets/{dataset_info["repository"]}), which contains {plural_word(dataset_info["size"], "image")}.
                 * Batch size is {meta_info["train"]["dataset"]["bs"]}, resolution is {ds_res}x{ds_res}, clustering into {plural_word(meta_info["train"]["dataset"]["num_bucket"], "bucket")}.
                 * Batch size for regularization dataset is {meta_info["train"]["reg_dataset"]["bs"]}, resolution is {reg_res}x{reg_res}, clustering into {plural_word(meta_info["train"]["reg_dataset"]["num_bucket"], "bucket")}.
                 * Trained for {plural_word(meta_info["train"]["train"]["train_steps"], "step")}, {plural_word(len(meta_info["steps"]), "checkpoint")} were saved and evaluated.
