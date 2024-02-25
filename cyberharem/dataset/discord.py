f48f2a0f3b1a8537a08536635f7748f951316d43
narugo1992 <narugo992@gmail.com>
2024-02-26 02:05:59 +0800

    dev(narugo): add auto publish for datasets

--- cyberharem/dataset/discord.py
+++ cyberharem/dataset/discord.py
@@ -1,3 +1,4 @@
+import glob
 import io
 import json
 import os
@@ -6,7 +7,10 @@ import textwrap
 from discord_webhook import DiscordWebhook
 from hbutils.scale import size_to_bytes_str
 from hbutils.string import plural_word
+from hbutils.system import TemporaryDirectory
+from hfutils.operate import download_archive_as_directory
 from huggingface_hub import hf_hub_url
+from waifuc.source import LocalSource
 
 from cyberharem.utils import get_hf_fs
 
@@ -16,12 +20,12 @@ def publish_to_discord(repository: str):
     meta_info = json.loads(hf.read_text(f'datasets/{repository}/meta.json'))
 
     hf_url = f'https://huggingface.co/datasets/{repository}'
-    with io.StringIO()as sio:
+    with io.StringIO() as sio:
         print(textwrap.dedent(f"""
             Dataset of `{meta_info['display_name']}` has been published to huggingface repository: {hf_url}.
             * The trigger word expected is `{meta_info['name']}`.
             * Core tags of this model: `{', '.join(meta_info['core_tags'])}`.
-            * This dataset contains {plural_word(meta_info['base_size'], 'image')} as base.
+            * **This dataset contains {plural_word(meta_info['base_size'], 'image')} as base**.
             * {plural_word(len(meta_info['clusters']), 'cluster')} detected in this dataset.
         """).strip(), file=sio)
 
@@ -35,4 +39,22 @@ def publish_to_discord(repository: str):
             url=os.environ['DC_DATASET_WEBHOOK'],
             content=sio.getvalue(),
         )
+        with TemporaryDirectory() as td:
+            origin_dir = os.path.join(td, 'source')
+            os.makedirs(origin_dir, exist_ok=True)
+            download_archive_as_directory(
+                repo_id=repository,
+                file_in_repo='dataset-raw.zip',
+                local_directory=origin_dir,
+                repo_type='dataset',
+            )
+
+            dst_dir = os.path.join(td, 'dst')
+            os.makedirs(dst_dir, exist_ok=True)
+            LocalSource(origin_dir)[:10].export(dst_dir)
+
+            for img_file in glob.glob(os.path.join(dst_dir, '*.png')):
+                with open(img_file, 'rb') as f:
+                    webhook.add_file(file=f.read(), filename=os.path.basename(img_file))
+
         webhook.execute()
