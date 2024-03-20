b9ba95c553ea92a29f588a7dfb229e0f87af57e4
narugo1992 <narugo992@gmail.com>
2024-03-20 23:27:34 +0800

    dev(narugo): more importants

--- cyberharem/train/__init__.py
+++ cyberharem/train/__init__.py
@@ -1,2 +1,2 @@
 from .reg import RegDataset, bangumi_reg_dataset, default_reg_dataset, prepare_reg_dataset
-from .train import train_lora
+from .train import train_lora, set_kohya_from_venv_dir, set_kohya_from_conda_dir
