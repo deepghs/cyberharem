46ba0144f64220ebccd65a776ea186209c5475bf
narugo1992 <narugo992@gmail.com>
2024-03-22 00:37:53 +0800

    dev(narugo): iterate all steps

--- cyberharem/publish/steps.py
+++ cyberharem/publish/steps.py
@@ -37,16 +37,19 @@ def find_steps_in_workdir_dep(workdir: str) -> Tuple[str, List[int]]:
 
 def find_steps_in_workdir(workdir: str) -> pd.DataFrame:
     data = []
+    exist_steps = set()
     for file in glob.glob(os.path.join(workdir, 'kohya', '*.safetensors')):
         meta = read_metadata(file)
         step = int(meta['ss_steps'])
-        data.append({
-            'step': step,
-            'epoch': int(meta['ss_epoch']),
-            'name': os.path.splitext(os.path.basename(file))[0],
-            'filename': os.path.basename(file),
-            'file': os.path.abspath(file),
-        })
+        if step not in exist_steps:
+            exist_steps.add(step)
+            data.append({
+                'step': step,
+                'epoch': int(meta['ss_epoch']),
+                'name': os.path.splitext(os.path.basename(file))[0],
+                'filename': os.path.basename(file),
+                'file': os.path.abspath(file),
+            })
     df = pd.DataFrame(data)
     df = df.sort_values(['step'], ascending=[True])
     return df
