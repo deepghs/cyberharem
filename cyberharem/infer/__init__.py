347f9317577f3192418caf6ee489f5b4f460e632
narugo1992 <narugo992@gmail.com>
2024-03-22 03:34:35 +0800

    dev(narugo): add new code

--- cyberharem/infer/__init__.py
+++ cyberharem/infer/__init__.py
@@ -1,3 +1,3 @@
 from .draw import draw_images, draw_images_for_workdir, list_rtags, list_rtag_names
 from .steps import find_steps_in_workdir
-from .webui import set_webui_server, set_webui_local_dir, LoraMock, LocalLoraMock
+from .webui import set_webui_server, set_webui_local_dir, LoraMock, LocalLoraMock, infer_with_lora, infer_with_workdir
