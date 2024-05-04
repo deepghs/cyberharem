889c56fa741caaafa6257ae11b5b0afb16dab556
narugo1992 <narugo992@gmail.com>
2024-05-04 23:10:27 +0800

    dev(narugo): add infer_for_scale

--- cyberharem/infer/__init__.py
+++ cyberharem/infer/__init__.py
@@ -1,3 +1,4 @@
 from .steps import find_steps_in_workdir
 from .tags import find_tags_from_workdir
-from .webui import set_webui_server, set_webui_local_dir, LoraMock, LocalLoraMock, infer_with_lora, infer_with_workdir
+from .webui import set_webui_server, set_webui_local_dir, LoraMock, LocalLoraMock, infer_with_lora, \
+    infer_with_workdir, infer_for_scale
