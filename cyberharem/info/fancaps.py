36c9a2e9e898ea0aaeab8795d1b5cae65aee2912
narugo1992 <narugo992@gmail.com>
2024-07-19 17:13:05 +0800

    dev(narugo): x

--- cyberharem/info/fancaps.py
+++ cyberharem/info/fancaps.py
@@ -1,5 +1,7 @@
 import os
+from pprint import pprint
 
+import numpy as np
 import pandas as pd
 from ditk import logging
 from hfutils.cache import delete_detached_cache
@@ -18,15 +20,19 @@ def get_fancaps_bangumis():
         os.path.relpath(path, f'datasets/{fancaps_repo}') for path in
         hf_fs.glob(f'datasets/{fancaps_repo}/tables/table-*.csv')
     ]
+    bangumi_x = {}
     for path in fs_path:
         for df_chunk in tqdm(pd.read_csv(hf_client.hf_hub_download(
                 repo_id=fancaps_repo,
                 repo_type='dataset',
                 filename=path,
         ), chunksize=10000), desc=f'Read Chunk {path!r}'):
-            print(df_chunk)
-            print(df_chunk.columns)
-            quit()
+            df_chunk = df_chunk.replace(np.NaN, None)
+            for item in df_chunk.to_dict('records'):
+                if item['bangumi_id'] not in bangumi_x:
+                    logging.info(f'Bangumi found #{item["bangumi_id"]}, name: {item["bangumi_name"]!r}')
+                    bangumi_x[item['bangumi_id']] = item['bangumi_name']
+    pprint(bangumi_x)
 
 
 if __name__ == '__main__':
