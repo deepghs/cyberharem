e825ddadd20870b1fef0ed487db0fdbfce6c0a93
narugo1992 <narugo992@gmail.com>
2025-01-07 15:34:26 +0800

    dev(narugo): subs

--- cyberharem/info/subsplease.py
+++ cyberharem/info/subsplease.py
@@ -91,7 +91,7 @@ def sync(repository: str):
     episodes = []
     animes = []
 
-    for ei, show_item in tqdm(list(enumerate(page('#main .all-shows .all-shows-link').items()))[:],
+    for ei, show_item in tqdm(list(enumerate(page('#main .all-shows .all-shows-link').items()))[:10],
                               desc='All Animes'):
         url = urljoin(resp.url, show_item('a').attr('href'))
         title = show_item('a').text().strip()
@@ -290,5 +290,5 @@ def sync(repository: str):
 if __name__ == '__main__':
     logging.try_init_root(logging.INFO)
     sync(
-        repository='deepghs/subsplease_animes',
+        repository='deepghs/subsplease_animes_x',
     )
