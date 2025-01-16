d1ddeee925413f7e86fb3e255abf5073166dd696
narugo1992 <narugo992@gmail.com>
2025-01-16 16:20:54 +0800

    dev(narugo): add episode newest

--- cyberharem/info/subsplease.py
+++ cyberharem/info/subsplease.py
@@ -1,4 +1,5 @@
 import datetime
+import io
 import os.path
 import re
 import time
@@ -97,6 +98,62 @@ def _get_image_url(image_dict: dict):
         return None
 
 
+def get_info_from_subsplease(page_url: str):
+    logging.info(f'Accessing page {page_url!r} ...')
+    resp = session.get(page_url)
+    resp.raise_for_status()
+
+    assert urlsplit(page_url).path_segments[1] == 'shows'
+    page_id = urlsplit(page_url).path_segments[2]
+
+    page = pq(resp.text)
+    title = page('h1.entry-title').text().strip()
+    synopsis = page('.series-syn').text().strip()
+    cover_image_url = urljoin(resp.url, page('#site-sidebar img.img-center').attr('src')) \
+        if page('#site-sidebar img.img-center').attr('src') else None
+
+    with io.StringIO() as sf:
+        print(page('div.entry-content').text().strip(), file=sf)
+        print(f'', file=sf)
+
+        sid_value = page('#show-release-table').attr('sid')
+        if sid_value:
+            logging.info(f'Getting release list of sid {sid_value!r} ...')
+            r = session.get('https://subsplease.org/api/', params={
+                'f': 'show', 'tz': 'Asia/Tokyo', 'sid': sid_value,
+            })
+            r.raise_for_status()
+
+            batch = r.json().get('batch') or {}
+            if batch:
+                print('Batch', file=sf)
+                print(f'', file=sf)
+                for key, item in batch.items():
+                    print(f'#{item["episode"]} - {key!r} - {item["release_date"]}', file=sf)
+                print(f'', file=sf)
+
+            episode = r.json().get('episode') or {}
+            if episode:
+                print('Episodes', file=sf)
+                print(f'', file=sf)
+                for key, item in episode.items():
+                    print(f'#{item["episode"]} - {key!r} - {item["release_date"]}', file=sf)
+                print(f'', file=sf)
+
+        prompt = sf.getvalue()
+
+    return {
+        'page_id': page_id,
+        'url': resp.url,
+        'title': title,
+        'prompt': prompt,
+        'cover_image_url': cover_image_url,
+        'synopsis': synopsis,
+        'batch': list(batch.values()),
+        'episode': list(episode.values()),
+    }
+
+
 def sync(repository: str):
     if not hf_client.repo_exists(repo_id=repository, repo_type='dataset'):
         hf_client.create_repo(repo_id=repository, repo_type='dataset', exist_ok=True)
@@ -121,7 +178,8 @@ def sync(repository: str):
             continue
 
         collected_nyaasi_ids = set()
-        for episode_item in full_item['subsplease']['episode']:
+        ex = get_info_from_subsplease(url)
+        for episode_item in ex['episode']:
             for ditem in episode_item['downloads']:
                 torrent_url = ditem['torrent']
                 assert urlsplit(torrent_url).path_segments[1] == 'view'
